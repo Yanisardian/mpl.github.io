f84b2f20d1c2262cb5548d83021f1d1c666bef35
Yanisardian <yanisardian8@gmail.com>
2025-03-26 05:55:08 -0700

    pembaruan destop

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    alert("Selamat datang di Mahasiswa Peduli Lingkungan Universitas Hamzanwadi!");
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector("nav ul");
+
+    menuToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
 });
